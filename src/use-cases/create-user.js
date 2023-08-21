4a4e2cea88503240d849b7abac25a5af96d0561c
leonan123 <leonanorth3@gmail.com>
2023-08-21 02:21:59 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcrypt'
+import { v4 as uuidv4 } from 'uuid'
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    //TODO: Verificar se o email ja esta em uso
+
+    const userId = uuidv4()
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
+
+    const user = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    }
+
+    const postgresCreateUserRepository = new PostgresCreateUserRepository()
+    const createdUser = await postgresCreateUserRepository.execute(user)
+
+    return createdUser
+  }
+}
