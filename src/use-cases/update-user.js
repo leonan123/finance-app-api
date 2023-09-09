f57efd50b0045fadd57986755a18ddc159c5aa9b
leonan123 <leonanorth3@gmail.com>
2023-09-09 17:57:30 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,32 @@
+import bcrypt from 'bcrypt'
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+
+export class UpdateUserUseCase {
+  async execute(userId, updateUserParams) {
+    if (updateUserParams.email) {
+      const postgresGetUserByEmailRepository = new PostgresGetUserByEmailRepository()
+      const userWithProvidedEmail = await postgresGetUserByEmailRepository.execute(updateUserParams.email)
+
+      if (userWithProvidedEmail) {
+        throw new EmailAlreadyInUseError(updateUserParams.email)
+      }
+    }
+
+    const user = {
+      ...updateUserParams,
+    }
+
+    if (updateUserParams.password) {
+      const hashedPassword = await bcrypt.hash(updateUserParams.password, 10)
+
+      user.password = hashedPassword
+    }
+
+    const updateUserRepository = new PostgresUpdateUserRepository()
+    const updatedUser = await updateUserRepository.execute(userId, user)
+
+    return updatedUser
+  }
+}
