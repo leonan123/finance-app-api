05dc2d8c35fe12c4387fd9cd826f15ec1cc57b69
leonan123 <leonanorth3@gmail.com>
2023-12-18 00:34:23 -0300

    refactor: id validation removed, as it is being done in the controller

--- src/use-cases/transaction/delete-transaction.js
+++ src/use-cases/transaction/delete-transaction.js
@@ -1,20 +1,9 @@
-import {
-  checkIfIdIsValid,
-  invalidIdResponse,
-} from '../../controllers/helpers/index.js'
-
 export class DeleteTransactionUseCase {
   constructor(deleteTransactionRepository) {
     this.deleteTransactionRepository = deleteTransactionRepository
   }
 
   async execute(transactionId) {
-    const idIsValid = checkIfIdIsValid(transactionId)
-
-    if (!idIsValid) {
-      return invalidIdResponse()
-    }
-
     const transactionDeleted =
       await this.deleteTransactionRepository(transactionId)
 
